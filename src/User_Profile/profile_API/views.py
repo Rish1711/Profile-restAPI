00bccb717ddb9384274a233c4f2d21bd6ceba852
Rishabh Gupta <rg171195@gmail.com>
2020-05-14 01:19:43 +0530

    Created GET and POST method

--- src/User_Profile/profile_API/views.py
+++ src/User_Profile/profile_API/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from . import serializers
+from rest_framework import status
 
 # Create your views here.
 
 class Hello_APIView(APIView):
     """ Test API View """
+    serializer_class = serializers.HelloSerializer
 
     def get(self,request,format=None):
         """ Return list of feature offered by API View """
@@ -16,3 +19,13 @@ class Hello_APIView(APIView):
         'It mapped manually to URLs'
         ]
         return Response({'message':'Hello Rishabh','api_view':api_view})
+
+    def post(self,request):
+        serializer=serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
